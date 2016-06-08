57ba6ef0d429f120283e7f1053a79a518cfc5bee
Mateu Cànaves <mateu.canaves@gmail.com>
2016-06-08 12:01:11 +0200

    Release 0.4.0

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -6,9 +6,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import Client, TestCase
 
-from .testapp.views import (
-    instance_view, model_form_view, model_from_db_view, model_view
-)
+from .testapp.views import instance_view, model_from_db_view, model_view
 
 
 class TestView(TestCase):
@@ -40,56 +38,17 @@ class TestView(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-        self.assertContains(response, 'money|0.0 JPY|')
-        self.assertContains(response, 'money.amount|0.0|')
+        self.assertContains(response, 'money|0 JPY|')
+        self.assertContains(response, 'money.amount|0|')
         self.assertContains(response, 'money.currency|JPY|')
 
     @pytest.mark.skipif('sqlite' in settings.DATABASES['default']['ENGINE'],
                         reason='SQLite can not represent trailing zeros')
     def test_model_from_db_view_trailing_zeros(self):
-        url = reverse(model_from_db_view, kwargs={'amount': '542.100', 'currency': 'JPY'})
+        url = reverse(model_from_db_view, kwargs={'amount': '542.100', 'currency': 'USD'})
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, 'money|JPY 542.100|')
-        self.assertContains(response, 'money.amount|542.100|')
-        self.assertContains(response, 'money.currency|JPY|')
-
-
-class TestEditView(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_form_GET(self):
-        url = reverse(model_form_view, kwargs={'amount': '987.00', 'currency': 'JPY'})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, 'value="987.00"')
-        self.assertContains(response, '<option value="JPY" selected="selected">JPY - Yen</option>')
-
-    def test_form_GET_zero(self):
-        url = reverse(model_form_view, kwargs={'amount': '0.0', 'currency': 'JPY'})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, 'value="0.0"')
-        self.assertContains(response, '<option value="JPY" selected="selected">JPY - Yen</option>')
-
-        # TODO: fix test
-        # def test_form_POST(self):
-        #     url = reverse(model_form_view, kwargs={'amount': '555.5', 'currency': 'JPY'})
-        #
-        #     # We intentionally use decimals with a typically non-decimal currency
-        #     response = self.client.post(url, {
-        #         'name': 'ABC',
-        #         'price_0': '555.5',
-        #         'price_1': 'JPY',
-        #     })
-        #
-        #     # Find the object we created...
-        #     obj = SimpleMoneyModel.objects.last()
-        #     self.assertEqual(str(obj.price), '555.5 JPY')
-        #
-        #     self.assertContains(response, '|item:name|value:ABC|')
-        #     self.assertContains(response, '|item:price|value:555.5 JPY|')
+        self.assertContains(response, 'money|542.10 USD|')
+        self.assertContains(response, 'money.amount|542.10|')
+        self.assertContains(response, 'money.currency|USD|')
