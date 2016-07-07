7ec1de9a5c9f9225ff2ba589e64c04f366d4b12a
Mateu Cànaves <mateu.canaves@gmail.com>
2016-07-07 12:17:25 +0200

    Style fix

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
 
-from tests.testapp.views import *  # noqa
+from tests.testapp.views import instance_view, model_view, model_from_db_view
 
 urlpatterns = [
     url(r'^instance-view/$', instance_view),
