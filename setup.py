31ab915f8140c6aea23a4d969a3d45777cacb897
Mateu Cànaves <mateu.canaves@gmail.com>
2017-01-31 17:14:58 +0100

    Adjust python versions to django supported versions

--- setup.py
+++ setup.py
@@ -74,6 +74,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
     ],
 )
