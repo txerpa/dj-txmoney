261ddd8bca353cdfa53af2f76f5fea3ce1e91c3f
Sinisa <sinishad@gmail.com>
2018-09-18 12:27:02 +0200

    Added missing django1.9 constant QUERY_TERMS since it does not exist is django2.1

---
+++ txmoney/money/constants.py
@@ -0,0 +1,6 @@
+QUERY_TERMS = {
+    'exact', 'iexact', 'contains', 'icontains', 'gt', 'gte', 'lt', 'lte', 'in',
+    'startswith', 'istartswith', 'endswith', 'iendswith', 'range', 'year',
+    'month', 'day', 'week_day', 'hour', 'minute', 'second', 'isnull', 'search',
+    'regex', 'iregex',
+}
